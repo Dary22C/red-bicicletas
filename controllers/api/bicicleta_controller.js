f62696fbf60ef7ad4d6ba57939a280563a13d790
Dary22C <pinedamabel78@gmail.com>
2026-06-25 14:48:30 -0600

    Update bicicleta_controller.js

--- controllers/api/bicicleta_controller.js
+++ controllers/api/bicicleta_controller.js
@@ -1,58 +1,66 @@
 var Bicicleta = require('../../models/bicicleta');
 
 // GET /api/bicicletas
-exports.bicicleta_list = function (req, res) {
-    Bicicleta.allBicis(function (err, bicis) {
-        if (err) return res.status(500).send(err);
+exports.bicicleta_list = async function (req, res) {
+    try {
+        var bicis = await Bicicleta.allBicis();
         res.status(200).json({ bicicletas: bicis });
-    });
+    } catch (err) {
+        res.status(500).send(err);
+    }
 };
 
 // POST /api/bicicletas/create
-exports.bicicleta_create = function (req, res) {
-    var bici = {
-        code: req.body.code,
-        color: req.body.color,
-        modelo: req.body.modelo,
-        ubicacion: [req.body.lat, req.body.lng]
-    };
+exports.bicicleta_create = async function (req, res) {
+    try {
+        var bici = {
+            code: req.body.code,
+            color: req.body.color,
+            modelo: req.body.modelo,
+            ubicacion: [req.body.lat, req.body.lng]
+        };
 
-    Bicicleta.add(bici, function (err, newBici) {
-        if (err) return res.status(500).send(err);
+        var newBici = await Bicicleta.add(bici);
         res.status(201).json({ bicicleta: newBici });
-    });
+    } catch (err) {
+        res.status(500).send(err);
+    }
 };
 
 // GET /api/bicicletas/:code
-exports.bicicleta_get_by_code = function (req, res) {
-    Bicicleta.findByCode(req.params.code, function (err, bici) {
-        if (err) return res.status(500).send(err);
+exports.bicicleta_get_by_code = async function (req, res) {
+    try {
+        var bici = await Bicicleta.findByCode(req.params.code);
         if (!bici) return res.status(404).json({ msg: 'No existe bicicleta con ese code' });
         res.status(200).json({ bicicleta: bici });
-    });
+    } catch (err) {
+        res.status(500).send(err);
+    }
 };
 
 // PUT /api/bicicletas/update/:code
-exports.bicicleta_update = function (req, res) {
-    Bicicleta.findByCode(req.params.code, function (err, bici) {
-        if (err) return res.status(500).send(err);
+exports.bicicleta_update = async function (req, res) {
+    try {
+        var bici = await Bicicleta.findByCode(req.params.code);
         if (!bici) return res.status(404).json({ msg: 'No existe bicicleta con ese code' });
 
         bici.color = req.body.color;
         bici.modelo = req.body.modelo;
         bici.ubicacion = [req.body.lat, req.body.lng];
 
-        bici.save(function (err) {
-            if (err) return res.status(500).send(err);
-            res.status(200).json({ bicicleta: bici });
-        });
-    });
+        await bici.save();
+        res.status(200).json({ bicicleta: bici });
+    } catch (err) {
+        res.status(500).send(err);
+    }
 };
 
 // DELETE /api/bicicletas/delete/:code
-exports.bicicleta_delete = function (req, res) {
-    Bicicleta.removeByCode(req.params.code, function (err) {
-        if (err) return res.status(500).send(err);
+exports.bicicleta_delete = async function (req, res) {
+    try {
+        await Bicicleta.removeByCode(req.params.code);
         res.status(204).send();
-    });
+    } catch (err) {
+        res.status(500).send(err);
+    }
 };
